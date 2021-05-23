f30c849e19d99e4209680df9debc33fbb3d90d19
Khyati0107 <78732262+Khyati0107@users.noreply.github.com>
2021-05-23 11:08:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+const database = firebase.database();
+var balloon,balloonImage1,balloonImage2;
+// create database and position variable here
+var position;
+
+
+function preload(){
+   bg =loadImage("cityImage.png");
+   balloonImage1=loadAnimation("hotairballoon1.png");
+   balloonImage2=loadAnimation("hotairballoon1.png","hotairballoon1.png",
+   "hotairballoon1.png","hotairballoon2.png","hotairballoon2.png",
+   "hotairballoon2.png","hotairballoon3.png","hotairballoon3.png","hotairballoon3.png");
+  }
+
+//Function to set initial environment
+function setup() {
+  
+  
+  createCanvas(1500,700);
+
+  balloon=createSprite(250,450,150,150);
+  balloon.addAnimation("hotAirBalloon",balloonImage1);
+  balloon.scale=0.5;
+
+
+  var balloonPosition = database.ref('balloon/height');
+  balloonPosition.on("value", readHeight, showError);
+  
+  textSize(20); 
+}
+
+// function to display UI
+function draw() {
+  background(bg);
+
+  if(keyDown(LEFT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+      writePosition(-1,0);
+    }
+  else if(keyDown(RIGHT_ARROW)){
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+      updateHeight(1,0);
+  }
+
+  else if(keyDown(UP_ARROW)){
+    updateHeight (0,10)
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale = balloon.scale + 0.1
+      writePosition(0,-1);
+    }
+ 
+
+
+    else if(keyDown(DOWN_ARROW)){
+      updateHeight (0,-10)
+    balloon.addAnimation("hotAirBalloon",balloonImage2);
+    balloon.scale = balloon.scale -0.01
+      writePosition(0,+1);
+    }
+
+
+
+  drawSprites();
+  fill(0);
+  stroke("white");
+  textSize(25);
+  text("**Use arrow keys to move Hot Air Balloon!",40,40);
+}
+
+function updateHeight(x,y){
+  database.ref('balloon/height').set({
+    'x': height.x + x ,
+    'y': height.y + y
+  })
+}
+
+function readHeight(data){
+    height = data.val();
+    console.log(height.x);
+    ballon.x = height.x;
+    ballon.y = height.y;
+}
+
+function showError(){
+  console.log("Error in writing to the database");
+}
